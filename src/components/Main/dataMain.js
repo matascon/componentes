58c3866bcbc40df39c983f7b9ccb49db97d17bbe
matascon <tascon1010100@gmail.com>
2025-10-11 16:28:07 +0200

    I've added a new image of softskill, expressJS

--- src/components/Main/dataMain.js
+++ src/components/Main/dataMain.js
@@ -1,88 +1,98 @@
 export const knowledge = [
   {
-    name: 'HTML5',
-    route: '/images/html.png'
+    name: "HTML5",
+    route: "/images/html.png",
   },
   {
-    name: 'CSS',
-    route: '/images/css.png'
+    name: "CSS",
+    route: "/images/css.png",
   },
   {
-    name: 'Javascript',
-    route: '/images/js.png'
+    name: "Javascript",
+    route: "/images/js.png",
   },
   {
-    name: 'Typescript',
-    route: '/images/typescript.png'
+    name: "Typescript",
+    route: "/images/typescript.png",
   },
   {
-    name: 'Bootstrap',
-    route: '/images/bootstrap.png'
+    name: "React",
+    route: "/images/react.png",
   },
   {
-    name: 'Vite',
-    route: '/images/vite.png'
+    name: "Bootstrap",
+    route: "/images/bootstrap.png",
   },
   {
-    name: 'Java',
-    route: '/images/java.png'
+    name: "Vite",
+    route: "/images/vite.png",
   },
   {
-    name: 'Mysql',
-    route: '/images/mysql.png'
+    name: "Java",
+    route: "/images/java.png",
   },
   {
-    name: 'Git',
-    route: '/images/git.png'
+    name: "Mysql",
+    route: "/images/mysql.png",
   },
   {
-    name: 'Docker',
-    route: '/images/docker.png'
-  }
+    name: "Git",
+    route: "/images/git.png",
+  },
+  {
+    name: "Docker",
+    route: "/images/docker.png",
+  },
 ];
 
 export const learning = [
   {
-    name: 'React',
-    route: '/images/react.png'
+    name: "Tailwind",
+    route: "/images/tailwind.png",
   },
   {
-    name: 'Tailwind',
-    route: '/images/tailwind.png'
+    name: "NodeJS",
+    route: "/images/nodejs.png",
   },
   {
-    name: 'NodeJS',
-    route: '/images/nodejs.png'
+    name: "ExpressJS",
+    route: "/images/express.png",
   },
   {
-    name: 'Spring',
-    route: '/images/spring.png'
-  }
+    name: "Spring",
+    route: "/images/spring.png",
+  },
 ];
 
 export const studies = [
   {
-    name_es: 'Técnico de Software',
-    name_en: 'Software Technician',
+    name_es: "Técnico de Software",
+    name_en: "Software Technician",
     school: `<span>&lt;/</span>42Madrid - Telefónica<span>&gt;</span>`,
-    date: '2020 - 2021',
-    description_es: 'Un año en el cual asenté mis bases en el desarrollo de software gracias a programar en C y C++, y desarrollé una habilidad muy importante en este sector, logré convertir la frustración en una herramienta para mantenerme en constante aprendizaje.',
-    description_en: 'A year in which I solidified my foundations in software development thanks to programming in C and C++, and i developed a very important hability in IT sector, i achived turning frustration into a powerful tool to keep myself in constant learning.'
+    date: "2020 - 2021",
+    description_es:
+      "Un año en el cual asenté mis bases en el desarrollo de software gracias a programar en C y C++, y desarrollé una habilidad muy importante en este sector, logré convertir la frustración en una herramienta para mantenerme en constante aprendizaje.",
+    description_en:
+      "A year in which I solidified my foundations in software development thanks to programming in C and C++, and i developed a very important hability in IT sector, i achived turning frustration into a powerful tool to keep myself in constant learning.",
   },
   {
-    name_es: 'FP de Grado Superior en Desarrollo Web',
-    name_en: 'Higher Vocational Training Program in Web Development',
+    name_es: "FP de Grado Superior en Desarrollo Web",
+    name_en: "Higher Vocational Training Program in Web Development",
     school: `<span>&lt;/</span>Prometeo by ThePower<span>&gt;</span>`,
-    date: '2024 - 2026',
-    description_es: 'Solidificar conocimientos en desarrollo web tanto del lado del cliente como del lado del servidor haciendo uso de tecnologías como HTML, CSS, Javascript, Java y Mysql',
-    description_en: 'To solidify web development knowledge on both the client and server sides, using technologies such as HTML, CSS, JavaScript, Java, and MySQL.'
+    date: "2024 - 2026",
+    description_es:
+      "Solidificar conocimientos en desarrollo web tanto del lado del cliente como del lado del servidor haciendo uso de tecnologías como HTML, CSS, Javascript, Java y Mysql",
+    description_en:
+      "To solidify web development knowledge on both the client and server sides, using technologies such as HTML, CSS, JavaScript, Java, and MySQL.",
   },
   {
-    name_es: 'Master en Desarrollo Web Full-Stack',
+    name_es: "Master en Desarrollo Web Full-Stack",
     name_en: `Master's Degree in Full-Stack Web Development`,
     school: `<span>&lt;/</span>ThePower<span>&gt;</span>`,
-    date: '2024 - 2026',
-    description_es: 'Construir aplicaciones web utilizando tecnologías y librerías modernas como React, NodeJs, Express, MongoDB',
-    description_en: 'Building web applications using modern technologies and libraries such as React, Node.js, Express, and MongoDB.'
-  }
+    date: "2024 - 2026",
+    description_es:
+      "Construir aplicaciones web utilizando tecnologías y librerías modernas como React, NodeJs, Express, MongoDB",
+    description_en:
+      "Building web applications using modern technologies and libraries such as React, Node.js, Express, and MongoDB.",
+  },
 ];
